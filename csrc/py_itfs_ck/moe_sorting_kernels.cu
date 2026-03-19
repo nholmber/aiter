50aaa62a79427c91813eb82ef9afe8bc1fd70f33
Xun Wang <xunwang2@amd.com>
2026-03-19 03:06:28 -0500

    fix use after free issue in moe_sorting_fwd

--- csrc/py_itfs_ck/moe_sorting_kernels.cu
+++ csrc/py_itfs_ck/moe_sorting_kernels.cu
@@ -33,12 +33,13 @@ void moe_sorting_fwd(torch::Tensor& topk_ids,          // [m, topk]
     const hipStream_t stream = at::hip::getCurrentHIPStream();
 
     int workspace_size = moe_sorting_get_workspace_size(num_tokens, num_experts, topk, dispatch_policy);
-    void* ws_ptr       = nullptr;
+    torch::Tensor ws;
+    void* ws_ptr = nullptr;
     if(workspace_size > 0)
     {
-        auto ws = torch::empty({workspace_size},
+        ws     = torch::empty({workspace_size},
                                torch::TensorOptions().dtype(dtype).device(device_of(topk_ids)));
-        ws_ptr  = ws.data_ptr();
+        ws_ptr = ws.data_ptr();
     }
 
     moe_sorting(
